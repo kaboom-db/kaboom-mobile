fba673586d5a77b4246699b819a0002d9868dd06
Tanveer Najib <tanveernajib@gmail.com>
2022-04-08 14:48:45 +0100

    remove kotlin files

--- app/src/main/java/com/example/kaboommobile/models/Network.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kaboommobile.models
-
-data class Network(
-    val date_created: String,
-    val id: Int,
-    val logo: String,
-    val name: String,
-    val website: String
-)
